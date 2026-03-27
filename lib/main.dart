c0d7875926e4d236d8c79770b86fdb100bc8e0b5
Mohamed Fares <mofares010@gmail.com>
2026-03-27 20:35:58 +0200

    add pakage flutter bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_pagination/home/presintation/view/pagination_page.dart';
+import 'package:test_pagination/home/presintation/view/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PaginationPage(),
+      home: const HomePage(),
     );
   }
 }
