fc663cafedb1b1fb5c9117dc5804575bc074c1e6
Mohamed Fares <mofares010@gmail.com>
2026-03-27 20:34:24 +0200

    add logic pagination in product repo

--- lib/home/data/repo/product_repo.dart
+++ lib/home/data/repo/product_repo.dart
@@ -2,18 +2,20 @@ import 'package:dio/dio.dart';
 import 'package:test_pagination/home/data/models/product_model.dart';
 
 abstract class ProductRepo {
-  Future<List<ProductModel>> getProducts();
+  Future<List<ProductModel>> getProducts(int limit, int skip);
 }
 
 class ProductRepoImpl implements ProductRepo {
   final Dio dio;
   ProductRepoImpl({required this.dio});
   @override
-  Future<List<ProductModel>> getProducts() async {
+  Future<List<ProductModel>> getProducts(int limit, int skip) async {
     try {
-      final response = await dio.get("https://dummyjson.com/products?limit=10");
+      final response = await dio.get(
+        "https://dummyjson.com/products?limit=$limit&skip=$skip",
+      );
       final data = response.data["products"];
-      return data.map((e) => ProductModel.fromJson(e)).toList();
+      return data.map<ProductModel>((e) => ProductModel.fromJson(e)).toList();
     } catch (e) {
       throw Exception(e.toString());
     }
