c399e067d747b607dd53bb4e807e611768eae87c
Mohamed Fares <mofares010@gmail.com>
2026-03-27 18:28:16 +0200

    test pagination with out api

--- lib/pagination_page.dart
+++ lib/pagination_page.dart
@@ -8,8 +8,65 @@ class PaginationPage extends StatefulWidget {
 }
 
 class _PaginationPageState extends State<PaginationPage> {
+  final ScrollController controller = ScrollController();
+  List<int> items = List.generate(15, (index)=> index);
+  bool isLoading = false ;
+  @override
+  void initState() {
+
+    controller.addListener(_onScroll);
+    super.initState();
+  }
+
+  void _onScroll() {
+    if (isLoading) return;
+      final pixel = controller.position.pixels ;
+      final max  = controller.position.maxScrollExtent;
+      print("pixel : $pixel ,  max : $max");
+
+      if(pixel >= max - 200){
+        print("Load more");
+        moreSroll();
+      }
+  }
+  void moreSroll() {
+  setState(() {
+    isLoading = true;
+  });
+  Future.delayed(Duration(seconds: 2),(){
+    setState(() {
+      isLoading = false ;
+      items.addAll(List.generate(15, (index)=>items.length + index));
+    });
+  });
+
+  }
+  @override
+  void dispose() {
+    controller.dispose();
+    controller.removeListener(_onScroll);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pagination")),
+      body: ListView.builder(
+        controller: controller,
+        itemCount: items.length + (isLoading ? 1 :0),
+
+        itemBuilder: (context, index) {
+          if(index == items.length){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListTile(title: Text("Item $index"));
+        },
+      ),
+    );
   }
+
+
 }
